d6b282f8bf4526e282d9713be400bcb054c7adce
Gabriel Ferreira <105852359+Gabrielferreiracosta@users.noreply.github.com>
2022-11-22 11:52:27 +0000

    Updates

---
+++ src/img/components/second.js
@@ -0,0 +1,48 @@
+import React from "react"
+import './global.css';
+
+function Second() {
+    return (
+
+        <div>
+            <div className='flex items-center w-[1075px] absolute left-[183px] top-[970px] gap-[50px] py-5 rounded-md bg-[#61b1fb]/50'>
+                <img alt="Gabriel" src="./img/foto2_site-1.jpeg" className='flex-grow-0 flex-shrink-0 w-[350px] h-[437.69px] object-cover' />
+                <div className='flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[538px] relative gap-[35px] pl-5 pr-[433px]'>
+                    <h3 className='flex-grow-0 flex-shrink-0 w-[585px] h-[29px] text-lg font-Anton text-left text-white'>
+                        Quem somos nós?
+                    </h3>
+                    <p className='flex-grow-0 flex-shrink-0 w-[469px] text-base font-regular text-left text-white'>
+                        <span className='flex-grow-0 flex-shrink-0 w-[469px] text-base font-regular text-left text-white'>
+                            Meu nome é Gabriel, vindo de Pernambuco, sou Nordestino! Resolvi desbravar novos
+                            horizontes e acabei aqui em Portugal desde 2018. A facilidade de viajar na Europa me fez
+                            despertar um desejo imenso de conhecer cada pedaço desse planeta que vivemos.
+                        </span>
+                        <br /><br />
+                        <span className='flex-grow-0 flex-shrink-0 w-[469px] text-base font-roboto text-left text-white'>
+                            Sobre viagens? Até agora conto com 6 países, bem longe do meu amigo Iran. Brincadeiras a
+                            parte, a vida é um sopro e como muitos dizem por isso devemos aproveitar para conhecer ao máximo. Então embarque com a gente nessa aventura e vamos desbravar
+                            juntos esse mundo por meio dos nossos canais.
+                        </span>
+                        <br />
+                        <span className='flex-grow-0 flex-shrink-0 w-[469px] text-base font-regular text-left text-white'>
+                        </span >
+                        <br />
+                        <span className='flex-grow-0 flex-shrink-0 w-[469px] text-base font-regular text-left text-white'>
+                            Quer conhecer um pouquinho mais sobre tudo isso? Se liga então no blog de viagens e todas
+                            as dicas que passamos para vocês.</span>
+                            <br /><br />
+                        <span className='flex-grow-0 flex-shrink-0 w-[469px] text-base font-regular font-roboto text-left text-white'>
+                            Aproveita e acompanha a gente nas redes sociais e no Youtube!
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Second
+
+
+
