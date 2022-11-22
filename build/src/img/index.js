9fd8a06558138e2225c4443623d83f6fd936f0c6
Gabriel Ferreira <105852359+Gabrielferreiracosta@users.noreply.github.com>
2022-11-22 12:33:36 +0000

    test

---
+++ build/src/img/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App  from './App.js';
+
+
+ReactDOM.render(
+  <div>
+    <App></App>
+  </div>,
+  document.getElementById('root'),
+)
+
+
