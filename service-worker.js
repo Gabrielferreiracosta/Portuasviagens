a8d652d3550b943793a59a1192b43ee784928437
Gabriel Ferreira <105852359+Gabrielferreiracosta@users.noreply.github.com>
2022-12-07 20:50:10 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portuasviagens/precache-manifest.cb24db8ebe66a8a24fc880c7badda62f.js"
+  "/Portuasviagens/precache-manifest.5c3feecce674fb1c9d08f5e4ffa43146.js"
 );
 
 workbox.clientsClaim();
