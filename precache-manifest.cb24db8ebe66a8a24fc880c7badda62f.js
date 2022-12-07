c3e650717cba5c2473c8eb17e5c3378829aea679
Gabriel Ferreira <105852359+Gabrielferreiracosta@users.noreply.github.com>
2022-12-07 15:34:45 +0000

    Updates

---
+++ precache-manifest.cb24db8ebe66a8a24fc880c7badda62f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c608504800c77afb0f27",
+    "url": "/Portuasviagens/static/css/main.a2f1fce2.chunk.css"
+  },
+  {
+    "revision": "c608504800c77afb0f27",
+    "url": "/Portuasviagens/static/js/main.6c31eecf.chunk.js"
+  },
+  {
+    "revision": "3d11dea34e7cf3ab48a0",
+    "url": "/Portuasviagens/static/js/runtime~main.92f03fdc.js"
+  },
+  {
+    "revision": "897098ef682bdc50382c",
+    "url": "/Portuasviagens/static/js/2.b5747810.chunk.js"
+  },
+  {
+    "revision": "1dba53db71a42ffa92a453d16ab68d9b",
+    "url": "/Portuasviagens/index.html"
+  }
+];
