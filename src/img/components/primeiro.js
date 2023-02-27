28c2edf671ba0d558e15cf78ced429ec35fc6b56
Gabriel Ferreira <105852359+Gabrielferreiracosta@users.noreply.github.com>
2023-02-27 19:25:18 +0000

    Updates

--- src/img/components/primeiro.js
+++ src/img/components/primeiro.js
@@ -1,5 +1,5 @@
 import React from "react"
-import './public/src/components/global.css';
+import './global.css';
 
 function Primeiro() {
     return (
--- src/img/index.js
+++ src/img/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import App  from './App.js'
-import './global.css'
+import React from 'react';
+import App  from './App.js';
+import './index.css';
 
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-      
-      <>
+
       <App></ App>
-      </> 
+   
 );
 
