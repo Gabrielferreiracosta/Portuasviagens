04191768ad2f331e4843514c2ef9cdbcfc153d87
Gabriel Ferreira <105852359+Gabrielferreiracosta@users.noreply.github.com>
2023-08-15 17:42:08 +0100

    changes

---
+++ build/src/img/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App  from './App.js';
+import './index.css';
+
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(
+      <>
+
+      <App></ App>
+      </>
+);
+
